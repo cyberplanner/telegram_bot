4bbb0b30e43b2e33d31e09d803225c2439722caf
cyberplanner <sal@salweb.co.uk>
2016-11-23 21:26:30 +0000

    Tyding up.

--- asTelegram.rb
+++ asTelegram.rb
@@ -6,14 +6,14 @@ puts "Server is running!"
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
+    when '/start'
+     bot.api.send_message(chat_id: message.chat.id, text: "I am the SitePoint bot, My commands are /tutorial, /asTelegram, /Alwin and /map")
     when '/asTelegram'
       bot.api.send_message(chat_id: message.chat.id, text: "Welcome the BBC www.bbc.co.uk")
     when '/Alwin'
       bot.api.send_message(chat_id: message.chat.id, text: "Howdy Alwin!")
-    when '/start'
-     bot.api.send_message(chat_id: message.chat.id, text: "I am the SitePoint bot, My commands are /sitepoint /map")
-    when '/sitepoint'
-     bot.api.send_message(chat_id: message.chat.id, text: "Welcome to http://sitepoint.com")
+    when '/tutorial'
+     bot.api.send_message(chat_id: message.chat.id, text: "Hi, this is the tutorial used to create this bot: https://www.sitepoint.com/quickly-create-a-telegram-bot-in-ruby")
     when '/map'
      bot.api.send_location(chat_id: message.chat.id, latitude: 51.272410, longitude: 0.190898)
     end
