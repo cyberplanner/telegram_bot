b5e1e42b9ab9768c918687bead4f6fd0ccbb497f
alwinruby <alwinsolanky@gmail.com>
2016-11-23 20:58:52 +0000

    Can run the server

--- asTelegram.rb
+++ asTelegram.rb
@@ -1,2 +1,15 @@
 require 'dotenv'
+require 'telegram/bot'
 Dotenv.load
+token = ENV["TOKEN"]
+puts "Server is running!"
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/asTelegram'
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome the BBC www.bbc.co.uk")
+    when '/Alwin'
+      bot.api.send_message(chat_id: message.chat.id, text: "Howdy Alwin!")
+    end
+  end
+end
