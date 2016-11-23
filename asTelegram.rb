589fb8b344f7ee912fe89b61e08f1f8070cce113
alwinruby <alwinsolanky@gmail.com>
2016-11-23 21:07:46 +0000

    Few more tests

--- asTelegram.rb
+++ asTelegram.rb
@@ -10,6 +10,12 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Welcome the BBC www.bbc.co.uk")
     when '/Alwin'
       bot.api.send_message(chat_id: message.chat.id, text: "Howdy Alwin!")
+    when '/start'
+     bot.api.send_message(chat_id: message.chat.id, text: "I am the SitePoint bot, My commands are /sitepoint /map")
+    when '/sitepoint'
+     bot.api.send_message(chat_id: message.chat.id, text: "Welcome to http://sitepoint.com")
+    when '/map'
+     bot.api.send_location(chat_id: message.chat.id, latitude: 51.272410, longitude: 0.190898)
     end
   end
 end
